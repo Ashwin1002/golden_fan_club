5beb006bddb237d438714d6eacdde5fe653c5057
Ashwin1002 <ashwin.shrestha2258@gmail.com>
2025-03-24 17:00:06 +0545

    build and deploy to docs folder for hosting url from github

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "/golden_fan_club/"
--- src/gencontent.py
+++ src/gencontent.py
@@ -1,16 +1,16 @@
 import os
 from markdown_blocks import markdown_to_html_node
 
-def generate_pages_recursive(dir_path_content, template_path, dir_path_public):
+def generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath):
     for root, _, files in os.walk(dir_path_content):
         for file in files:
             if file.endswith(".md"):
                 from_path = os.path.join(root, file)
                 rel_path = os.path.relpath(from_path, dir_path_content)
                 dest_path = os.path.join(dir_path_public, os.path.splitext(rel_path)[0] + ".html")
-                generate_page(from_path, template_path, dest_path)
+                generate_page(from_path, template_path, dest_path, basepath)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -26,6 +26,7 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
